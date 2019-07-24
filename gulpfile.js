0f3a3363185af2bfa698b704b17f758386bb066b
Kevin Pessetto <kjpessetto@gmail.com>
2019-07-24 12:32:47 -0500

    undid changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 "use strict";
-ghpages = require('gulp-gh-pages');
+
 // Load plugins
 const autoprefixer = require("gulp-autoprefixer");
 const browsersync = require("browser-sync").create();
@@ -121,10 +121,6 @@ function watchFiles() {
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
-gulp.task('deploy', function() {
-  return gulp.src(options.dist + '**/*')
-             .pipe(ghpages());
-}
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
